e026f6aff10bf1bf59bee4cddb50effbcb2b7bcf
Oleg Harchuck <tetris66613@gmail.com>
2018-01-13 06:19:28 +0200

    Fix problen with mysql utf8mb4 character set not applying for varchars with length more then 250 that need for email

--- migrations/m171124_140441_create_table_users.php
+++ migrations/m171124_140441_create_table_users.php
@@ -7,10 +7,24 @@ class m171124_140441_create_table_users extends Migration
 {
     public function safeUp()
     {
+        switch ($this->db->driverName) {
+            case 'mysql':
+                // host problem: mysql server issue, not allowing more than 1000 bytes on email
+                // column when use utfmb4 character set
+                // most popular collate in some time for mysql would be utf8mb4
+                // ?that affect for bytes checks in mysql server?
+                // for email we need use unique contraint and email in standard
+                // can contain more bytes that limits apply
+                $email = 'VARCHAR(254) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL UNIQUE';
+                break;
+            default:
+                $email = $this->string(254)->notNull()->unique();
+        }
+
         $this->createTable(User::tableName(), [
             'id' => $this->primaryKey(),
             'username' => $this->string(32)->unique(),
-            'email' => $this->string(256)->notNull()->unique(),
+            'email' => $email,
             'password' => $this->text()->notNull(),
             'role' => $this->integer()->notNull(),
             'authkey' => $this->text()->notNull(),
